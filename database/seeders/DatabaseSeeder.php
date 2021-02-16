6d9de36ed6acdcdb28cb90138c45d57ddbf70805
Ryan Dawkes <rd@moose.co>
2021-02-16 17:50:47 +0000

    create user & roles seeds

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        // Run seed
+        $this->call([
+            RoleTableSeeder::class,
+            UserTableSeeder::class,
+        ]);
     }
 }
